4c165aac0699f7a5a91838ab9fd9ab8f977a7d57
Adara <adarahv@gmail.com>
2017-01-04 08:01:29 +0300

    initial commit

---
+++ spec/css-color-name-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CssColorNameView from '../lib/css-color-name-view';
+
+describe('CssColorNameView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
