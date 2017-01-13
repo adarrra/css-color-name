4ba45ab4c62e4c4d7951744498603c4ff192c368
Adara <adarahv@gmail.com>
2017-01-13 09:03:51 +0300

    try to deal with specs...

--- spec/css-color-name-spec.js
+++ spec/css-color-name-spec.js
@@ -2,20 +2,35 @@
 
 import Provider from '../lib/provider';
 
-describe('Provider', () => {
-    beforeEach(() => {
-      activationPromise = atom.packages.activatePackage('css-color-name');
-      openCssPromise = atom.workspace.open('test.css');
+describe('when we open a stylefile', () => {
+    // beforeEach(() => {
 
-      waitsForPromise(() => {
-        return activationPromise;
-      });
-      waitsForPromise(() => {
-        return openCssPromise;
-      });
-    });
-
-    it('activate css file', () => {
-        expect(atom.packages.isPackageActive('css-color-name')).toBe(true);
+    it('should be activated', () => {
+        waitsForPromise(() => {
+            alert(11111111111111);
+            atom.workspace.open('test.css')
+            .then((editor) => {
+                alert('inside');
+                expect(atom.packages.isPackageActive('css-color-name')).toBe(true);
+            })
+        });
     });
 });
+
+// describe('Provider', () => {
+//     beforeEach(() => {
+//       activationPromise = atom.packages.activatePackage('css-color-name');
+//       openCssPromise = atom.workspace.open('test.css');
+// 
+//       waitsForPromise(() => {
+//         return activationPromise;
+//       });
+//       waitsForPromise(() => {
+//         return openCssPromise;
+//       });
+//     });
+// 
+//     it('activate css file', () => {
+//         expect(atom.packages.isPackageActive('css-color-name')).toBe(true);
+//     });
+// });
