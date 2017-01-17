b87b96e4d446021a86e4c225f8f538ab78210163
Adara <adarahv@gmail.com>
2017-01-17 08:18:29 +0300

    struggle with specs

--- spec/css-color-name-spec.js
+++ spec/css-color-name-spec.js
@@ -2,35 +2,41 @@
 
 import Provider from '../lib/provider';
 
-describe('when we open a stylefile', () => {
-    // beforeEach(() => {
-
-    it('should be activated', () => {
-        waitsForPromise(() => {
-            alert(11111111111111);
-            atom.workspace.open('test.css')
-            .then((editor) => {
-                alert('inside');
-                expect(atom.packages.isPackageActive('css-color-name')).toBe(true);
-            })
-        });
-    });
-});
-
-// describe('Provider', () => {
-//     beforeEach(() => {
-//       activationPromise = atom.packages.activatePackage('css-color-name');
-//       openCssPromise = atom.workspace.open('test.css');
+// describe('when we open a stylefile', () => {
+//     // beforeEach(() => {
 // 
-//       waitsForPromise(() => {
-//         return activationPromise;
-//       });
-//       waitsForPromise(() => {
-//         return openCssPromise;
-//       });
-//     });
-// 
-//     it('activate css file', () => {
-//         expect(atom.packages.isPackageActive('css-color-name')).toBe(true);
+//     it('should be activated', () => {
+//         waitsForPromise(() => {
+//             atom.workspace.open('test.css')
+//             .then((editor) => {
+//                 console.log('editor');
+//                 alert('pause');
+//                 expect(atom.packages.isPackageActive('css-color-name')).toBe(true);
+//             })
+//         });
 //     });
 // });
+
+// atom --test --timeout 60 ./spec/css-color-name-spec.js
+// https://github.com/XuXuDev/ceekey-atom/blob/8b5ad74a2743927c8429a7add1fb16ea1ab704f4/packages/atom-ternjs/spec/atom-ternjs-spec.js
+// https://github.com/atom/autocomplete-plus/tree/master/spec
+
+describe('activated when we open a stylefile', () => {
+    beforeEach(() => {
+
+      waitsForPromise(() => {
+        return atom.workspace.open('test.css');
+      });
+    //   waitsForPromise(() => {
+    //     return atom.packages.activatePackage('css-color-name');
+    //   });
+      
+    });
+
+
+    it('activate css file', () => {
+        expect(atom.workspace.getActiveTextEditor().getPath()).toContain('test.css');
+        // expect(atom.packages.isPackageActive('css-color-name')).toBe(true);
+
+    });
+});
